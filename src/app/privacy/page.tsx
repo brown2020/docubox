1a693410e9f79d3aa1817a7f60fd689884350539
Steve Brown <brown2020@gmail.com>
2024-09-26 16:17:40 -0700

    name update

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -3,7 +3,7 @@ import PrivacyPage from "@/components/PrivacyPage";
 export default function page() {
   return (
     <PrivacyPage
-      companyName="Generate.me"
+      companyName="Docubox.ai"
       companyEmail="info@ignitechannel.com"
       companyAddress={"30765 Pacific Coast Hwy #354"}
       companyLocation={"Malibu, CA"}
